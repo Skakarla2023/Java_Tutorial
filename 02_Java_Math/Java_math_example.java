e499ce232ad2bb090dc1805d17343ac83e10d7e7
Skakarla2023 <skakarla2023@gmail.com>
2025-07-01 17:26:48 +0530

    Create Java_math_example.java

---
+++ 02_Java_Math/Java_math_example.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Math.max() returns the maximum value
+        System.out.println(Math.max(10,4));
+        
+        // Math.min() returns the minimum value
+        System.out.println(Math.min(78,10));
+        
+        // sqrt() to find the square root of a number
+        System.out.println(Math.sqrt(99));
+        
+        // returns the abstract(positive) value of the given number.
+        System.out.println(Math.abs(-8.29845));
+    }
+}
